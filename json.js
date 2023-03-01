504225da10ea5dbbc3aa19829477ed0539ee08c1
Mamoka1104 <109488428+Mamoka1104@users.noreply.github.com>
2023-03-01 16:56:49 +1000

    add Домание задание № 10.

---
+++ json.js
@@ -0,0 +1,59 @@
+const dataInfo =
+    `[{
+    "qotd_date": "2023-03-02T00:00:00.000+00:00",
+    "quote": {
+      "id": 38775,
+      "dialogue": false,
+      "private": false,
+      "tags": [
+        "love"
+      ],
+      "url": "https://favqs.com/quotes/george-bernard-shaw/38775-the-love-of-e-",
+      "favorites_count": 0,
+      "upvotes_count": 1,
+      "downvotes_count": 0,
+      "author": "George Bernard Shaw",
+      "photo_src": "https://www.nobelprize.org/images/shaw-12939-portrait-medium.jpg",
+      "author_permalink": "george-bernard-shaw",
+      "body": "The love of economy is the root of all virtue."
+    }
+  },
+  {
+    "qotd_date": "2023-03-02T00:00:00.000+00:00",
+    "quote": {
+      "id": 483,
+      "dialogue": false,
+      "private": false,
+      "tags": [
+        "integrity",
+        "fairness"
+      ],
+      "url": "https://favqs.com/quotes/eleanor-roosevelt/483-it-is-not-fair--",
+      "favorites_count": 1,
+      "upvotes_count": 1,
+      "downvotes_count": 0,
+      "author": "Eleanor Roosevelt",
+      "photo_src": "https://cdn.britannica.com/78/19578-050-E720BE72/Eleanor-Roosevelt-1950.jpg",
+      "author_permalink": "eleanor-roosevelt",
+      "body": "It is not fair to ask of others what you are not willing to do yourself."
+    }
+  },
+  {
+    "qotd_date": "2023-03-02T00:00:00.000+00:00",
+    "quote": {
+      "id": 27333,
+      "dialogue": false,
+      "private": false,
+      "tags": [
+        "good"
+      ],
+      "url": "https://favqs.com/quotes/buddha/27333-the-virtues-l-",
+      "favorites_count": 3,
+      "upvotes_count": 2,
+      "downvotes_count": 0,
+      "author": "Buddha",
+      "author_permalink": "buddha",
+      "photo_src": "https://www.drawingskill.com/wp-content/uploads/1/Gautama-Buddha-Realistic-Drawing.jpg",
+      "body": "The virtues, like the Muses, are always seen in groups. A good principle was never found solitary in any breast."
+    }
+  }]`
